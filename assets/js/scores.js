f17c8e68b91494cc9240b1285cfb0734d87134e5
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2023-12-05 23:38:25 +0000

    addedscores.js

---
+++ assets/js/scores.js
@@ -0,0 +1,14 @@
+// Function to save the score and initials
+function saveScore(initials, score) {
+    // Implement logic to save the user's initials and score, and update the high scores list
+  }
+  
+  // Function to clear high scores
+  function clearHighScores() {
+    // Implement logic to clear the high scores
+  }
+  
+  // Function to display high scores
+  function displayHighScores() {
+    // Implement logic to display the high scores on the page
+  }
